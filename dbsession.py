22c79694231619dd43bb48893a08b5fb28c86231
Tonto <tonto@winblows>
2025-02-22 18:50:41 +0200

    Add bigger pool size and max overflow.

--- dbsession.py
+++ dbsession.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 _logger = logging.getLogger(__name__)
 
-engine = create_engine(os.getenv("SQL_URI"), echo=False)
+engine = create_engine(os.getenv("SQL_URI"), echo=False, pool_size=60, max_overflow=120)
 Base = declarative_base()
 
 session_maker = sessionmaker(engine)
