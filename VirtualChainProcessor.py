541e5b19291d7ba58cbed21d9dcf16cba328399a
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-07-27 22:22:57 +0300

    ...

--- VirtualChainProcessor.py
+++ VirtualChainProcessor.py
@@ -106,7 +106,7 @@ class VirtualChainProcessor(object):
         """
         Add known blocks to database
         """
-        if self.start_block and self.start_block["verboseData"].get("isChainBlock", False):
+        if self.start_block is not None and self.start_block["verboseData"].get("isChainBlock", False):
             _logger.info(f'VCP requested with start hash {self.start_hash}')
             resp = await self.client.request("getVirtualSelectedParentChainFromBlockRequest",
                                             {"startHash": self.start_hash,
