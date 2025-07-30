ccef3611d423335b809f9940949f35bc582b171c
Toni Lukkaroinen <toni.lukkaroinen@gmail.com>
2025-07-30 11:57:44 +0300

    Don't print the start_block in vcp

--- VirtualChainProcessor.py
+++ VirtualChainProcessor.py
@@ -106,7 +106,6 @@ class VirtualChainProcessor(object):
         """
         Add known blocks to database
         """
-        _logger.info(self.start_block)
         if self.start_block is not None and self.start_block["verboseData"].get("isHeaderOnly") != True:
             _logger.info(f'VCP requested with start hash {self.start_hash}')
             resp = await self.client.request("getVirtualSelectedParentChainFromBlockRequest",
